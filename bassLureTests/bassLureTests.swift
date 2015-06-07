56e905848986c6823e14b12d2db924860f1fc887
thebarron <barronlink@gmail.com>
2015-06-06 20:53:54 -0500

    Initial Commit

---
+++ bassLureTests/bassLureTests.swift
@@ -0,0 +1,36 @@
+//
+//  bassLureTests.swift
+//  bassLureTests
+//
+//  Created by Barron Link on 6/6/15.
+//  Copyright (c) 2015 Motive. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class bassLureTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
